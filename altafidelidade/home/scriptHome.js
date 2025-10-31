82963776ce75626871791cd98833cf40b9b3d5f5
Bruna Bento <brunabento016@gmail.com>
2025-10-30 21:30:42 -0300

    fix: links e menu ocuto categorias

--- altafidelidade/home/scriptHome.js
+++ altafidelidade/home/scriptHome.js
@@ -30,6 +30,27 @@ document.addEventListener("click", (e) => {
     }
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const btnCategorias = document.getElementById("btn-categorias");
+    const menuCategorias = document.getElementById("menu-categorias");
+    const fecharMenu = document.getElementById("fechar-menu");
+
+    btnCategorias.addEventListener("click", function(e) {
+        e.preventDefault();
+        menuCategorias.classList.add("active");
+    });
+
+    fecharMenu.addEventListener("click", function() {
+        menuCategorias.classList.remove("active");
+    });
+
+    // Fecha o menu ao clicar fora do conteúdo
+    menuCategorias.addEventListener("click", function(e) {
+        if (e.target === menuCategorias) {
+            menuCategorias.classList.remove("active");
+        }
+    });
+});
 // === PLACEHOLDER ANIMADO ===
 const searchInput = document.getElementById("search-input");
 const text = "Busque por marcas, categorias ou produtos";
